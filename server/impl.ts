794bc94c9f90fe830f7b756c2ab75434b6fe9ba0
Harsh Pandey <hpandey793@gmail.com>
2021-09-23 15:37:03 -0400

    don't use system for collisions

--- server/impl.ts
+++ server/impl.ts
@@ -2,13 +2,11 @@ import { Methods, Context } from "./.rtag/methods";
 import { UserData, Response } from "./.rtag/base";
 import {
   PlayerState,
-  PlayerName,
   ICreateGameRequest,
   IFireCannonRequest,
   ISetOrientationRequest,
   IJoinGameRequest,
 } from "./.rtag/types";
-import { Collisions } from "detect-collisions";
 import { InternalShip } from "./Ship";
 import { InternalCannonBall } from "./CannonBall";
 
@@ -21,17 +19,15 @@ type InternalState = {
 const MAP_WIDTH = 1200;
 const MAP_HEIGHT = 900;
 
-const system = new Collisions();
-
 export class Impl implements Methods<InternalState> {
   createGame(user: UserData, ctx: Context, request: ICreateGameRequest): InternalState {
-    return { ships: [createShip(user.name)], cannonBalls: [], updatedAt: 0 };
+    return { ships: [new InternalShip(user.name)], cannonBalls: [], updatedAt: 0 };
   }
   joinGame(state: InternalState, user: UserData, ctx: Context, request: IJoinGameRequest): Response {
     if (state.ships.find((s) => s.player === user.name) !== undefined) {
       return Response.error("Already joined");
     }
-    state.ships.push(createShip(user.name));
+    state.ships.push(new InternalShip(user.name));
     return Response.ok();
   }
   setOrientation(state: InternalState, user: UserData, ctx: Context, request: ISetOrientationRequest): Response {
@@ -77,32 +73,21 @@ export class Impl implements Methods<InternalState> {
       cannonBall.update(timeDelta);
       const { x, y } = cannonBall.body;
       if (x < 0 || y < 0 || x >= MAP_WIDTH || y >= MAP_HEIGHT) {
-        cannonBall.body.remove();
         state.cannonBalls.splice(idx, 1);
       }
       state.updatedAt = ctx.time();
     });
-    system.update();
     state.ships.forEach((ship) => {
       state.cannonBalls.forEach((cannonBall, idx) => {
         if (ship.player !== cannonBall.firedBy && ship.body.collides(cannonBall.body)) {
           ship.handleCollision();
           state.cannonBalls.splice(idx, 1);
-          cannonBall.body.remove();
         }
       });
     });
   }
 }
 
-function createShip(player: PlayerName) {
-  const ship = new InternalShip(player);
-  system.insert(ship.body);
-  return ship;
-}
-
 function createCannonBall(id: number, ship: InternalShip, dAngle: number) {
-  const cannonBall = new InternalCannonBall(id, ship.player, ship.body.x, ship.body.y, ship.body.angle + dAngle);
-  system.insert(cannonBall.body);
-  return cannonBall;
+  return new InternalCannonBall(id, ship.player, ship.body.x, ship.body.y, ship.body.angle + dAngle);
 }
